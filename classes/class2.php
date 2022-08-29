cea1b5c2d6cbb90d1899e6d3f94edfdb21769e30
Piyush <piyush.pandey@innoraft.com>
2022-08-29 10:56:50 +0530

    first commit

---
+++ classes/class2.php
@@ -0,0 +1,9 @@
+<?php
+namespace pp;
+class class2{
+    public function __construct()
+    {
+        echo __CLASS__;
+    }
+}
+?>
---
+++ classes/class3.php
@@ -0,0 +1,9 @@
+<?php
+namespace pp;
+class class3{
+    public function __construct()
+    {
+        echo __CLASS__;
+    }
+}
+?>
